57e1ac9180f41affabebdc470a1c80d27c5f0b1a
pixlperfect01 <44816788+pixlperfect01@users.noreply.github.com>
2018-12-16 12:51:50 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
+var ans = Math.trunc(Math.random()*10000);
 function check(){
 var inpt = document.getElementById('input');
 var btn = document.getElementById('btn');
 var div = document.getElementById('div');
-var ans = Math.trunc(Math.random()*10000);
 if(inpt.value==ans){
 div.hidden=false;
 console.log(ans+' was right!');
